7edf2f1ca58c1995c0f5164fb53bfe9e0f7359d5
jinxian.gong <jinxian.gong@aispeech.com>
2022-10-28 16:05:42 +0800

    2022年10月28日16:05:40

--- spring/src/main/java/com/xdroid/spring/example/TestExampleActivity.java
+++ spring/src/main/java/com/xdroid/spring/example/TestExampleActivity.java
@@ -14,6 +14,7 @@ import com.xdroid.spring.R;
 
 public class TestExampleActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    private static final String TAG2 = "MainActivity";
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
